b21b4c1ee9127ade3acd48b34e44049da3989a6f
aineza2 <a.ineza2@alustudent.com>
2024-02-25 21:40:55 +0200

    complete

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,55 +1,55 @@
 #!/usr/bin/python3
 """
-Exports user task data to a csv file
+Using what you did in the task #0,
+extend your Python script to export
+data in the JSON format.
+
+Requirements:
+
+~ Records all tasks from all employees
+~ Format must be: { "USER_ID": [ {"username":
+"USERNAME", "task": "TASK_TITLE", "completed":
+TASK_COMPLETED_STATUS}, {"username": "USERNAME",
+"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS},
+... ], "USER_ID": [ {"username": "USERNAME", "task":
+"TASK_TITLE", "completed": TASK_COMPLETED_STATUS},
+{"username": "USERNAME", "task": "TASK_TITLE",
+"completed": TASK_COMPLETED_STATUS}, ... ]}
+
+~ File name must be: todo_all_employees.json
 """
-from requests import get
-from json import dump
-
-
-def get_data(url):
-    """gets data from an api"""
-    request = get(url)
-    
-    if request.status_code == 200:
-        return request.json()
-    else:
-        raise Exception(request.status_code)
-
-
-def main():
-    """program starting point"""
-
-    # Get users
-    users_data_url = f'https://jsonplaceholder.typicode.com/users'
-    users = get_data(users_data_url)
-
-    # Create a user hashmap for faster lookup
-    users_hashmap = {}
-    for user in users:
-        user_id = user['id']
-        username = user['username']
-        users_hashmap[user_id] = username
-
-    # Get todos
-    todos_url = f'https://jsonplaceholder.typicode.com/todos'
-    todos = get_data(todos_url)
-
-    # Data object to write
-    data = {}
-
-    # Add todos to data object
-    for todo in todos:
-        user_id = todo["userId"]
-        task_data = {'username': users_hashmap[user_id], 'task': todo['title'], 'completed': todo['completed']}
-
-        if not user_id in data:
-            data[user_id] = []
-        
-        data[user_id].append(task_data)
-    
-    with open(f'todo_all_employees.json', 'w') as f:
-        dump(data, f)
-
-
 if __name__ == "__main__":
-    main()
+    import json
+    import requests
+    import sys
+    # create your api urls used for making the request
+    url1 = f"https://jsonplaceholder.typicode.com/users"
+    url2 = f"https://jsonplaceholder.typicode.com/todos"
+    req_rep1 = requests.get(url1)
+    req_rep2 = requests.get(url2)
+    req_rep1 = req_rep1.json()
+    req_rep2 = req_rep2.json()
+    # get the todo keys for the users
+    all_user_keys = req_rep2[0].keys()
+    # create a dict for storing all users
+    # username, title as task, completed
+    all_user_json = {}
+    user_count = 0
+    each_user_todo = 20
+    for i in range(0, len(req_rep2), each_user_todo):
+        new_list = []
+        for j in range(i, i+each_user_todo):
+            new_dict = {}
+            new_dict["username"] = req_rep1[user_count]["username"]
+            for key in all_user_keys:
+                if key != "title" and key != "completed":
+                    continue
+                if key == "title":
+                    new_dict["task"] = req_rep2[j][key]
+                else:
+                    new_dict["completed"] = req_rep2[j][key]
+            new_list.append(new_dict)
+        all_user_json[f'{req_rep1[user_count]["id"]}'] = new_list
+        user_count += 1
+    with open("todo_all_employees.json", 'w', encoding="utf-8") as jsonfile:
+        json.dump(all_user_json, jsonfile)
